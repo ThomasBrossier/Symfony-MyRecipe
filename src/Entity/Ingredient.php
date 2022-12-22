b7b223d12b84e8d099985e53414388335919d770
ThomasBrossier <98547958+ThomasBrossier@users.noreply.github.com>
2022-12-22 16:46:49 +0100

    add CRUD for Ingredients in admin dashBoard

--- src/Entity/Ingredient.php
+++ src/Entity/Ingredient.php
@@ -6,9 +6,11 @@ use App\Repository\IngredientRepository;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\HttpFoundation\File\File;
 use Symfony\Component\Validator\Constraints as Assert;
-
+use Vich\UploaderBundle\Mapping\Annotation as Vich;
 #[ORM\Entity(repositoryClass: IngredientRepository::class)]
+#[Vich\Uploadable]
 class Ingredient
 {
     #[ORM\Id]
@@ -27,12 +29,13 @@ class Ingredient
     private ?string $name = null;
 
     #[ORM\Column(length: 255)]
-    #[Assert\NotBlank]
     private ?string $slug = null;
 
     #[ORM\Column(length: 255)]
-    #[Assert\NotBlank]
     private ?string $picture = null;
+
+    #[Vich\UploadableField(mapping: 'ingredient', fileNameProperty: 'picture')]
+    private ?File $imageFile = null;
 
     #[ORM\OneToMany(mappedBy: 'ingredients', targetEntity: RecipeIngredient::class)]
     private Collection $recipeIngredients;
@@ -40,6 +43,9 @@ class Ingredient
     #[ORM\ManyToOne(inversedBy: 'ingredients')]
     #[ORM\JoinColumn(nullable: false)]
     private ?CategoryIngredient $category = null;
+
+    #[ORM\Column(nullable: true)]
+    private ?\DateTimeImmutable $updatedAt = null;
 
     public function __construct()
     {
@@ -88,6 +94,31 @@ class Ingredient
     }
 
     /**
+     * If manually uploading a file (i.e. not using Symfony Form) ensure an instance
+     * of 'UploadedFile' is injected into this setter to trigger the update. If this
+     * bundle's configuration parameter 'inject_on_load' is set to 'true' this setter
+     * must be able to accept an instance of 'File' as the bundle will inject one here
+     * during Doctrine hydration.
+     *
+     * @param File|null $imageFile
+     */
+    public function setImageFile(?File $imageFile = null): void
+    {
+        $this->imageFile = $imageFile;
+
+        if (null !== $imageFile) {
+            // It is required that at least one field changes if you are using doctrine
+            // otherwise the event listeners won't be called and the file is lost
+            $this->updatedAt = new \DateTimeImmutable();
+        }
+    }
+
+    public function getImageFile(): ?File
+    {
+        return $this->imageFile;
+    }
+
+    /**
      * @return Collection<int, RecipeIngredient>
      */
     public function getRecipeIngredients(): Collection
@@ -125,6 +156,18 @@ class Ingredient
     public function setCategory(?CategoryIngredient $category): self
     {
         $this->category = $category;
+
+        return $this;
+    }
+
+    public function getUpdatedAt(): ?\DateTimeImmutable
+    {
+        return $this->updatedAt;
+    }
+
+    public function setUpdatedAt(?\DateTimeImmutable $updatedAt): self
+    {
+        $this->updatedAt = $updatedAt;
 
         return $this;
     }
---
+++ src/Form/IngredientType.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\CategoryIngredient;
+use App\Entity\Ingredient;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\File;
+use Vich\UploaderBundle\Form\Type\VichImageType;
+
+class IngredientType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('name',TextType::class,[
+                'label'=> "Nom"
+            ])
+            ->add('imageFile', VichImageType::class,[
+                'label'=> "Photo de l'ingrédient",
+                'required'=>false,
+                'constraints' => [
+                    new File([
+                        'maxSize' => '2048k',
+                        'maxSizeMessage'=> "La taille de l\'image doit être inférieure à {{ limit }}",
+                        'mimeTypes' => [
+                            'image/png',
+                            'image/jpeg',
+                            'image/webp'
+                        ],
+                        'mimeTypesMessage' => 'Veuillez fournir une image valide',
+                    ])
+                ],
+            ])
+            ->add('category', EntityType::class,[
+                'label' => 'Catégorie de l\'ingrédient',
+                'class'=> CategoryIngredient::class,
+                'autocomplete' => true,
+                'choice_label' => 'name',
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Ingredient::class,
+        ]);
+    }
+}
